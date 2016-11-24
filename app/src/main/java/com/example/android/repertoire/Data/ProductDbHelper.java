3dcabe0ed66f38708452be66c1fc6d55c96aecab
erickvg <erickvernon@live.com>
2016-11-24 16:00:38 -0600

    chnages made in DB helper class

--- app/src/main/java/com/example/android/repertoire/Data/ProductDbHelper.java
+++ app/src/main/java/com/example/android/repertoire/Data/ProductDbHelper.java
@@ -12,7 +12,7 @@ import static com.example.android.repertoire.Data.ProductContract.ProductEntry;
 
 public class ProductDbHelper extends SQLiteOpenHelper{
 
-    public static final int DATABASE_VERSION = 3;
+    public static final int DATABASE_VERSION = 2;
     public static final String DATABASE_NAME = "inventory.db";
 
     public ProductDbHelper (Context context){
@@ -38,6 +38,11 @@ public class ProductDbHelper extends SQLiteOpenHelper{
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP IF TABLE EXISTS" + ProductEntry.TABLE_NAME);
+        onCreate(db);
+
+
 
     }
 
